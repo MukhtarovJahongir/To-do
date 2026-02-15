39ed8d17b2a8e022f616a6b6a996da3f0628ed58
MukhtarovJahongir <mukhtarovjahongir@gmail.com>
2026-02-15 13:56:22 +0500

    Render backend bilan moslashtirildi

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:1819/api",
+  baseURL: "https://to-do-beckend.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
